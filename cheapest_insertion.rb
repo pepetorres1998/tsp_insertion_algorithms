897f87d1804f413decfaa1bf21824fc42fc90275
Jose Antonio Torres Garibay <jtorres@syscap.com.mx>
2020-05-07 04:03:49 -0500

    Add cheapest insertion algorithm

--- cheapest_insertion.rb
+++ cheapest_insertion.rb
@@ -1,7 +1,46 @@
-class CheapestInsertion
-  def initialize(distances)
-    @distances = distances
-    @route = []
-    @total = 0
+require_relative 'farthest_insertion'
+
+class CheapestInsertion < FarthestInsertion
+  def initialize(distances, starting_node)
+    super
+  end
+
+  private
+
+  def posible_k_nodes
+    (0...distances.length).to_a - route.uniq
+  end
+
+  def start_algorithm
+    until posible_k_nodes.empty?
+      path = where_to_insert_k
+      insert_k(path)
+    end
+  end
+
+  def where_to_insert_k
+    k_nodes = posible_k_nodes.map do |k_node|
+      paths.map do |path|
+        {
+          k_node: k_node,
+          delta_f: distances[path[0]][k_node].to_i + \
+            distances[k_node][path[1]].to_i - \
+            distances[path[0]][path[1]].to_i,
+          path: [path[0], path[1]],
+        }
+      end
+    end.flatten(1)
+
+    minimum_delta_f = k_nodes.sort_by { |k_node| k_node[:delta_f] }.first
+
+    self.k_node = minimum_delta_f[:k_node]
+
+    minimum_delta_f[:path]
+  end
+
+  def get_first_node(starting_node)
+    distances_from_starting_node = distances[starting_node]
+    min_distance = distances_from_starting_node.select(&:positive?).min
+    distances_from_starting_node.find_index(min_distance)
   end
 end
--- main.rb
+++ main.rb
@@ -1,14 +1,37 @@
 require 'csv'
 require 'pry'
 require_relative 'farthest_insertion'
+require_relative 'cheapest_insertion'
 
-distances = CSV.read("./#{ARGV[0]}.csv").map { |row| row.map(&:to_i) }
+distances = CSV.read("./distances.csv").map { |row| row.map(&:to_i) }
+
 if ARGV[1].to_i.negative? || ARGV[1].to_i > distances.length
-  return puts 'La ciudad debe existir'
+  return puts 'La ciudad debe existir y estar en el archivo "distances.csv"'
 end
 
-farthest_insertion = FarthestInsertion.new(distances, ARGV[1].to_i - 1)
+case ARGV[0]
+when 'farthest'
+  farthest_insertion = FarthestInsertion.new(distances, [ARGV[1].to_i - 1, 0].max)
 
-puts "\n\nFarthest Insertion Algorithm\n\n\n"
-puts "TSP Route: #{farthest_insertion.print_route}\n"
-puts "Total Cost: #{farthest_insertion.total_cost}\n\n\n"
+  puts "\n\nFarthest Insertion Algorithm\n\n\n"
+  puts "TSP Route: #{farthest_insertion.print_route}\n"
+  puts "Total Cost: #{farthest_insertion.total_cost}\n\n\n"
+when 'cheapest'
+  cheapest_insertion = CheapestInsertion.new(distances, [ARGV[1].to_i - 1, 0].max)
+
+  puts "\n\nCheapest Insertion Algorithm\n\n\n"
+  puts "TSP Route: #{cheapest_insertion.print_route}\n"
+  puts "Total Cost: #{cheapest_insertion.total_cost}\n\n\n"
+when 'help'
+  puts "\n\nEjecute el archivo main.rb y agrege 2 argumentos, el primero para saber el
+  algoritmo deseado a ejecutar y el segundo para saber el nodo inicial\n\n\n"
+
+  puts "Ejemplos:\n\n
+  bundle exec ruby main.rb farthest 3\n\n
+  bundle exec ruby main.rb cheapest 1\n\n
+  bundle exec ruby main.rb cheapest\n\n
+  *Por default el valor del nodo inicial es 1\n\n"
+else
+  puts 'Ingresa una opcion de algoritmo valida'
+  return
+end

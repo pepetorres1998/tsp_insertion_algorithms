c2fdcd4f58a6765f8d6c378f9b6c9419b818239c
Jose Antonio Torres Garibay <jtorres@syscap.com.mx>
2020-05-07 00:46:20 -0500

    Add validation that starting node exists

--- main.rb
+++ main.rb
@@ -3,7 +3,11 @@ require 'pry'
 require_relative 'farthest_insertion'
 
 distances = CSV.read("./#{ARGV[0]}.csv").map { |row| row.map(&:to_i) }
-farthest_insertion = FarthestInsertion.new(distances, ARGV[1].to_i)
+if ARGV[1].to_i.negative? || ARGV[1].to_i > distances.length
+  return puts 'La ciudad debe existir'
+end
+
+farthest_insertion = FarthestInsertion.new(distances, ARGV[1].to_i - 1)
 
 puts "\n\nFarthest Insertion Algorithm\n\n\n"
 puts "TSP Route: #{farthest_insertion.print_route}\n"
